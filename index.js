b82ff8a769857078a300eb9df7fa588f4d518a48
Bilal Pervez <rajabilal555@gmail.com>
2022-07-24 20:15:10 +0500

    ipaddress fix

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get("/api/hello", function (req, res) {
 
 app.get("/api/whoami", function (req, res) {
 	res.json({
-		ipaddres: req.ip,
+		ipaddress: req.headers["x-forwarded-for"],
 		language: req.headers["accept-language"],
 		software: req.headers["user-agent"],
 	});
